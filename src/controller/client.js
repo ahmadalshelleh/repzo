b8fb048925ebc065af97fa85efcb45cf1fc89067
ahmad alshelleh <ahmadalshelleh@yahoo.com>
2022-02-05 20:20:25 +0200

    updates

--- src/controller/client.js
+++ src/controller/client.js
@@ -1,60 +1,49 @@
 const Client = require("../models/client");
 
 const getLessVisitedData = async ({ from, to, day }) => {
-    const days = {
-        sunday: 1,
-        monday: 2,
-        tuesday: 3,
-        wednesday: 4,
-        thursday: 5,
-        friday: 6,
-        saturday: 7,
-      };
-      const limit = { $limit: 5 };
-    
-      const lookup = {
-        $lookup: {
-          from: "visits",
-          let: {
-            id: "$_id",
-            from: parseInt(from),
-            to: parseInt(to),
-            day: days[day.toLowerCase()],
-          },
-          pipeline: [
-            {
-              $match: {
-                $expr: {
-                  $and: [
-                    { $eq: ["$$id", "$client"] },
-                    { $gte: ["$time", "$$from"] },
-                    { $lte: ["$time", "$$to"] },
-                    { $eq: [{ $dayOfWeek: { $toDate: "$time" } }, "$$day"] },
-                  ],
-                },
-              },
+  const lookup = {
+    $lookup: {
+      from: "visits",
+      let: {
+        id: "$_id",
+        from: parseInt(from),
+        to: parseInt(to),
+        day: day,
+      },
+      pipeline: [
+        {
+          $match: {
+            $expr: {
+              $and: [
+                { $eq: ["$$id", "$client"] },
+                { $gte: ["$time", "$$from"] },
+                { $lte: ["$time", "$$to"] },
+                { $eq: [{ $dayOfWeek: { $toDate: "$time" } }, "$$day"] },
+              ],
             },
-          ],
-          as: "client_visits",
+          },
         },
-      };
-    
-      const project = {
-        $project: {
-          name: 1,
-          number_of_visits: { $size: "$client_visits" },
-          client_visits: 1,
-        },
-      };
-    
-      const sort = { $sort: { number_of_visits: 1 } };
-    
-      try {
-        const clients = await Client.aggregate([limit, lookup, project, sort]);
-        return clients
-      } catch (e) {
-        return null
-      }
-}
+      ],
+      as: "client_visits",
+    },
+  };
 
-module.exports.getLessVisitedData = getLessVisitedData
+  const project = {
+    $project: {
+      name: 1,
+      number_of_visits: { $size: "$client_visits" },
+      client_visits: 1,
+    },
+  };
+
+  const sort = { $sort: { number_of_visits: 1 } };
+
+  try {
+    const clients = await Client.aggregate([lookup, project, sort]);
+    return clients;
+  } catch (e) {
+    return null;
+  }
+};
+
+module.exports.getLessVisitedData = getLessVisitedData;
--- src/middleware/params.js
+++ src/middleware/params.js
@@ -1,13 +1,24 @@
 const checkParams = async (req, res, next) => {
-    const params = ['from', 'to', 'day'];
-    let keys = Object.keys(req.query)
+  const params = ["from", "to", "day"];
+  const days = {
+    sunday: 1,
+    monday: 2,
+    tuesday: 3,
+    wednesday: 4,
+    thursday: 5,
+    friday: 6,
+    saturday: 7,
+  };
+  let keys = Object.keys(req.query);
 
-    const isValid = keys.every((update) => params.includes(update))
-    
-    if(keys.length !== 3 || !isValid)
-        res.status(400).send({ error: 'Params issue!' })
-    
-    next()
-} 
+  const isValid = keys.every((update) => params.includes(update));
 
-module.exports = checkParams
+  if (keys.length !== 3 || !isValid || !days[req.query.day]) {
+    res.status(400).send({ error: "Params issue!" });
+  } else {
+    req.query.day = days[req.query.day];
+    next();
+  }
+};
+
+module.exports = checkParams;
